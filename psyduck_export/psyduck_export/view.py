512bbab070829c196ea5b498b3c1f79dea5636f7
yinlong <yinlong@coolfishgames.com>
2019-07-19 19:03:10 +0800

    完善功能

--- psyduck_export/psyduck_export/view.py
+++ psyduck_export/psyduck_export/view.py
@@ -1,58 +1,106 @@
 from django.shortcuts import render
-from psyduck_export import helper
+from psyduck_export.helper import Helper
 from django.http import HttpResponse
 import json
 from threading import Thread
-import asyncio
+import time
 
 
 class User:
     uuid = ''
     thread = None
-    step = 0
+    helper = None
+    step = ''
     progress = 0
     msg = ''
+    signal = ''
+    args = {}
 
-    def __init__(self, **kwargs):
-        self.uuid = kwargs['uuid']
-        self.thread = kwargs['thread']
-        self.step = kwargs['step']
-        pass
+    def __init__(self, uuid):
+        self.uuid = uuid
 
 
 users: {str, User} = {}
 
 
-async def _async_export(uuid):
-    helper.init(uuid)
-    helper.auto_login()
-    async for item in helper.export_all():
-        print(item)
-    helper.dispose()
+def _export_thread(uuid):
+    u = users[uuid]
+    u.helper = Helper(uuid)
+    h: Helper = u.helper
+    u.step = 'start'
+    h.init()
+    u.step = 'login'
+    while not h.check_login():
+        while u.signal != 'login':
+            time.sleep(100)
+        u.signal = ''
+        h.login(u.args['phone_num'], u.args['verify_code'])
+    u.step = 'export'
+    while not u.signal != 'export':
+        time.sleep(100)
+    u.signal = ''
+    h.export_all()
+    u.step = 'dispose'
+    h.dispose()
 
 
-def async_export(uuid):
-    asyncio.set_event_loop(asyncio.new_event_loop())
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(_async_export(uuid))
+def _export_init(uuid):
+    users[uuid] = User(uuid)
+
+
+def _export_start(uuid):
+    users[uuid].thread = Thread(target=_export_thread, args=(uuid,))
+    users[uuid].thread.start()
+
+
+def _response(step, msg=''):
+    return HttpResponse(json.dumps({'step': step, 'msg': msg}), content_type='application/json')
 
 
 def export(request):
-    context = {'step': 0}
-    return render(request, 'index.html', context)
-
-
-def export_start(request):
-    uuid = request.GET.get('murmur', '')
-    t = Thread(target=async_export, args=(uuid,))
-    users[uuid] = User(uuid=uuid, step=1, thread=t)
-    context = {'step': 1}
+    context = {'step': 'need_init'}
     return render(request, 'index.html', context)
 
 
 def export_progress(request):
     uuid = request.GET.get('murmur', '')
+    act = request.GET.get('act', '')
+    if uuid == '':
+        return _response('none')
+
+    if act == 'init':
+        if uuid not in users.keys():
+            _export_init(uuid)
+        return _response('init')
+
     if uuid not in users.keys():
-        return HttpResponse(json.dumps({'step': 0}), content_type='application/json')
-    user = users[uuid]
-    return HttpResponse(json.dumps({'step': user.step}), content_type='application/json')
+        return _response('need_init')
+
+    u = users[uuid]
+    if act == 'start':
+        _export_start(uuid)
+        return _response('starting')
+
+    if act == 'starting':
+        if u.helper.is_ready:
+            if u.step == 'login':
+                return _response('login')
+            else:
+                return _response('export')
+        else:
+            return _response('starting')
+
+    if act == 'login':
+        if u.step == 'login':
+            return _response('login')
+        else:
+            return _response('export')
+
+    if act == 'export':
+        u.signal = 'export'
+        return _response('process')
+
+    if act == 'progress':
+        return _response('process')
+
+    return _response('none')
